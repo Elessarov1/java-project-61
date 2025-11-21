12d2db3d58b1fc1aa51b47ea668075f68e507a39
Alexandr Afanasev <aaa@weblock.su>
2025-11-21 16:32:31 +0300

    step 2: application plugin added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    application
     id("com.github.ben-manes.versions") version "0.53.0"
 }
 
@@ -8,6 +8,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+    applicationDefaultJvmArgs = listOf("-Dgreeting.language=en")
 }
 
 buildscript {
